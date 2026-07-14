53eef4fd4d8e8ac9d20db5350da9e12f606ab2b0
KatiaVasileva <eka.vasileva@gmail.com>
2026-07-14 10:36:40 +0500

    initial commit

---
+++ src/main/java/com/vasileva/finalprojectquest/FinalProjectQuestApplication.java
@@ -0,0 +1,13 @@
+package com.vasileva.finalprojectquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectQuestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectQuestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vasileva/finalprojectquest/FinalProjectQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasileva.finalprojectquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectQuestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
